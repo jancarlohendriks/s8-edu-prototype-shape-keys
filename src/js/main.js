4c6af92984502b610fd8163cc241004024690817
jancarlohendriks <jancarlohendriks@hotmail.com>
2023-06-02 14:41:14 +0200

    working animation

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as dat from "dat.gui";
-import model from "@/assets/models/Humans-2.gltf?url";
+import model from "@/assets/models/Humans-3.gltf?url";
 
 let camera = new THREE.PerspectiveCamera(
   50,
@@ -10,7 +10,7 @@ let camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(1.5, 3, 1.5);
+camera.position.set(5, 5, 5);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 let scene = new THREE.Scene();
@@ -44,27 +44,76 @@ let animate = () => {
 };
 
 let loader = new GLTFLoader();
-let human_regular;
 let gui = new dat.GUI();
-let options = {
+let guiControls = {
+  speed: 1.0,
   "human_short_small-base": 0,
   "human_short_large-base": 0,
 };
 
-loader.load(model, (gltf) => {
-  human_regular = gltf.scenes[0].children.find(
+// Load the GLTF file
+loader.load(model, function (gltf) {
+  const object = gltf.scene;
+  const animations = gltf.animations;
+  scene.add(object);
+
+  // Create an animation mixer array
+  const mixers = [];
+
+  // Play all animations
+  animations.forEach((animation) => {
+    const mixer = new THREE.AnimationMixer(object);
+    mixers.push(mixer);
+    const action = mixer.clipAction(animation);
+    action.play();
+  });
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    const delta = clock.getDelta();
+    mixers.forEach((mixer) => {
+      mixer.update(delta * guiControls.speed);
+    });
+
+    renderer.render(scene, camera);
+  }
+
+  const clock = new THREE.Clock();
+
+  let human_regular = gltf.scene.children[0].children.find(
     (obj) => obj.name == "human_short_regular-base"
   );
-  scene.add(human_regular);
-  console.log(human_regular.morphTargetInfluences);
 
   let morphChange = () => {
-    human_regular.morphTargetInfluences[0] = options["human_short_small-base"];
-    human_regular.morphTargetInfluences[1] = options["human_short_large-base"];
+    human_regular.morphTargetInfluences[0] =
+      guiControls["human_short_small-base"];
+    human_regular.morphTargetInfluences[1] =
+      guiControls["human_short_large-base"];
   };
 
-  gui.add(options, "human_short_small-base", 0, 1).onChange(morphChange);
-  gui.add(options, "human_short_large-base", 0, 1).onChange(morphChange);
+  gui.add(guiControls, "speed", 0.1, 2).step(0.1).name("Playback Speed");
+
+  const weightFolder = gui.addFolder("Weight");
+  weightFolder.open();
+  weightFolder
+    .add(guiControls, "human_short_small-base", 0, 1)
+    .name("Thin")
+    .onChange(morphChange);
+  weightFolder
+    .add(guiControls, "human_short_large-base", 0, 1)
+    .name("Big")
+    .onChange(morphChange);
+
+  animate();
 });
 
-animate();
+// UPDATE ON RESIZE
+window.addEventListener("resize", onWindowResize, false);
+window.addEventListener("load", onWindowResize, false);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+}
